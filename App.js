20e3bc24032b6f921a4491a7b143840965f764d8
Vigneshwar-N <vigneshwar2k2@gmail.com>
2024-10-08 11:00:08 +0530

    created the Whatsapp clone

---
+++ App.js
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import _Chats from './src/screens/_Chats';
+
+export default function App() {
+  return (
+    <View>
+      <_Chats />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
