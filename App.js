fb828f3636a752758f2d3a45ab0411d87e0d08bc
Vigneshwar-N <vigneshwar2k2@gmail.com>
2024-10-10 16:51:05 +0530

    working

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import _Chats from './src/screens/_Chats';
 import Header from './src/components/common/_Header';
 import StackNav from './src/navigation/StackNav';
 import {NavigationContainer} from '@react-navigation/native';
+import {ChatContext} from './Hooks/UseContext';
 
 export default function App() {
   return (
